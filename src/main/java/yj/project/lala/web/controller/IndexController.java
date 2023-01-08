1d7d78738e204c59b9f2c375f0bd55d8db4c47a0
ParkYoungJun <june0313@gmail.com>
2023-01-08 14:02:33 +0900

    IndexController 추가 - react router 경로가 스프링 컨트롤러를 호출하지 않도록

---
+++ src/main/java/yj/project/lala/web/controller/IndexController.java
@@ -0,0 +1,12 @@
+package yj.project.lala.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping(value = {"/", "/category" })
+    public String index() {
+        return "/index.html";
+    }
+}
